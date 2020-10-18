5e20e810dd941a9a851799060d00b57102dd8915
Braden Mars <bradenmars@bradenmars.me>
2020-10-17 20:17:46 -0500

    feat(app): cleanup app init, add hall/switch pins

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@
 """Black app"""
 
 import picoweb
+
 import models
 from black import Black
 from motor import Motor
@@ -10,6 +11,17 @@ from motor import Motor
 STEP_PIN = 25
 DIR_PIN = 26
 EN_PIN = 27
+
+HALL_PIN = 33
+SWITCH_PIN = 34
+
+
+def get_black():
+    models.db.connect()
+    models.Config.create_table(False)
+    motor = Motor(DIR_PIN, STEP_PIN, EN_PIN)
+    black = Black(motor, switch_pin=SWITCH_PIN, hall_pin=HALL_PIN)
+    return motor, black
 
 
 class BlackApp(picoweb.WebApp):
@@ -19,10 +31,8 @@ class BlackApp(picoweb.WebApp):
         self.events_sink = set()
 
     def init(self):
-        models.db.connect()
-        models.Config.create_table(False)
-        motor = Motor(DIR_PIN, STEP_PIN, EN_PIN)
-        self.black = Black(motor)
+        _, black = get_black()
+        self.black = black
         super().init()
 
 
